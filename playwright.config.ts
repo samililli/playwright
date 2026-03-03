47c52057267f9fa30b49b0697b400b7cff8943e0
Nada <nada.pernerova111@gmail.com>
2026-03-03 15:17:50 +0100

    slowmo: 500,

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,13 +24,11 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    // headless: false,
+ use: {
     trace: 'on-first-retry',
+    launchOptions: {
+      slowMo: 500,
+    },
   },
 
   /* Configure projects for major browsers */
